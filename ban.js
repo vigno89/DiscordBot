e354926eeb23a63e34439140e78d316cb8b7ec26
vigno89 <76709114+vigno89@users.noreply.github.com>
2021-04-13 09:34:17 -0400

    ban command update

--- ban.js
+++ ban.js
@@ -1,12 +1,12 @@
 exports.run = async (bot,message,args) => {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have permission to execute this command!")
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("tu peut pas faire cette command criss de pain!")
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-    if(!member) return message.channel.send("Invalid Member Given!");
-    if(member.roles.highest.position > message.member.roles.highest.position) return message.channel.send("You cannot ban someone with more power than you!")
+    if(!member) return message.channel.send("Pas le bon membre criss de pain!");
+    if(member.roles.highest.position > message.member.roles.highest.position) return message.channel.send("Tu peut pas le ban, il est meilleur que toi cheh!")
     let reason = args.slice(1).join(' ');
-    if(!reason) {reason = 'No reason provided'}
+    if(!reason) {reason = 'Aucune raison'}
     member.ban({reason: reason, days: 7})
-    message.channel.send(`**${member.user.tag}** has been banned for ${reason}`)
+    message.channel.send(`**${member.user.tag}** a été banni pour ${reason}`)
 }
 
 exports.help = {
